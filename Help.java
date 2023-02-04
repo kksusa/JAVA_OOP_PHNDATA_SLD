4b8f5f575841bd97073efab8082bc713df77aca5
kksusa <kksusa1@gmail.com>
2023-02-04 22:00:58 +0500

    Класс справки по командам

---
+++ Help.java
@@ -0,0 +1,10 @@
+public class Help {
+    /**Вызов справки по возможным командам*/
+    public static void Help(){
+        System.out.println("Для записи телефона и пользователя введите add.");
+        System.out.println("Для удаления телефона и пользователя введите delete.");
+        System.out.println("Для вывода списка телефонов и пользователей введите list.");
+        System.out.println("Для вывода списка команд введите help.");
+        System.out.println("Для выхода из программы введите exit.");
+    }
+};
